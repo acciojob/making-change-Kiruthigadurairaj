fb4fc4a1800ff6a64afc24c2467fc81b612583e0
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2024-12-27 19:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const makeChange = (c) => {
   // your name here
+	
+	c = parseInt(c);
+
+    const quarterValue = 25; 
+    const dimeValue = 10;    
+    const nickelValue = 5;   
+    const pennyValue = 1;    
+
+    const q = Math.floor(c / quarterValue); 
+    c %= quarterValue;                     
+
+    const d = Math.floor(c / dimeValue);   
+    c %= dimeValue;                        
+
+    const n = Math.floor(c / nickelValue); 
+    c %= nickelValue;                      
+
+    const p = c;                          
+    return { q, d, n, p };
+};
+
+console.log(makeChange(47)); 
+console.log(makeChange(24));
 };
 
 // Do not the change the code below
